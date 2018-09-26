2b4169a2a9a5c77f20205be7a04e085b0b062090
karthikeyanrama <karthishakith@gmail.com>
2018-09-26 11:30:54 +0530

    demo

---
+++ src/script/TestDemo.java
@@ -0,0 +1,14 @@
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+
+public class TestDemo extends BaseTest {
+	@Test
+	public void testA() {
+		Reporter.log("testA ",true);
+	}
+
+}
